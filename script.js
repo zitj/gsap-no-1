87fa0e69e66fddd9a03ff1a61c534d32ce84cb4e
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-03-21 18:55:37 +0100

    Add timeline

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-gsap.from('header', { duration: 1, y: '-100%', ease: 'bounce' });
-gsap.from('.link', { duration: 1, opacity: '0', delay: 0.7, stagger: 0.2 });
-gsap.from('.right', {
-    duration: 1,
-    x: '-100vw',
-    delay: 1.5,
-    ease: 'power2.in',
+const timeline = gsap.timeline({ defaults: { duration: 1 } });
+
+const animate = () => {
+    timeline
+        .from('header', { y: '-100%', ease: 'bounce' })
+        .from('.link', { opacity: '0', stagger: 0.5 })
+        .from('.right', { x: '-100vw', ease: 'power2.in' }, 1)
+        .from('.left', { x: '-100vw' }, '<.5')
+        .to('footer', { y: 0, ease: 'elastic' })
+        .fromTo(
+            'button',
+            { opacity: 0, scale: 0, rotation: 720 },
+            { opacity: 1, scale: 1, rotation: 0 }
+        );
+};
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    timeline.timeScale(5);
+    timeline.reverse();
 });
-gsap.from('.left', { duration: 1, x: '-100%', delay: 2, ease: 'power2.in' });
-gsap.to('footer', { duration: 1, y: 0, ease: 'elastic', delay: 3.5 });
+
+timeline.timeScale(4);
+animate();
